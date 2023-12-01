d8fea787e390a6c8b28c2e653c698061ec53494d
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2023-12-01 13:19:50 -0500

    potentially implemented the updateFavouritesCount

--- song-microservice/src/main/java/com/eecs3311/songmicroservice/SongController.java
+++ song-microservice/src/main/java/com/eecs3311/songmicroservice/SongController.java
@@ -83,7 +83,6 @@ public class SongController {
 	public ResponseEntity<Map<String, Object>> addSong(@RequestBody Map<String, String> params, HttpServletRequest request) {
 		Map<String, Object> response = new HashMap<>();
 		response.put("path", String.format("POST %s", Utils.getUrl(request)));
-
 		try {
 			String songName = params.get("songName");
 			String songArtistFullName = params.get("songArtistFullName");
@@ -92,7 +91,6 @@ public class SongController {
 				response.put("error", "Missing required parameters");
 				return Utils.setResponseStatus(response, null, null);
 			}
-
 			Song newSong = new Song(songName, songArtistFullName, songAlbum);
 			DbQueryStatus savedSong = songDal.addSong(newSong);
 			response.put("message", "Song added successfully");
@@ -100,23 +98,41 @@ public class SongController {
 			return Utils.setResponseStatus(response, savedSong.getdbQueryExecResult(), savedSong.getData());
 
 		} catch (Exception e) {
-			// Handle exceptions, log the error, and return an error response
 			response.put("error", "Internal Server Error");
 			return Utils.setResponseStatus(response, null, null);
 		}
 	}
 
 
-
-
-
 	@RequestMapping(value = "/updateSongFavouritesCount", method = RequestMethod.PUT)
 	public ResponseEntity<Map<String, Object>> updateFavouritesCount(@RequestBody Map<String, String> params, HttpServletRequest request) {
-
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("data", String.format("PUT %s", Utils.getUrl(request)));
-		// TODO: add any other values to the map following the example in getSongById
-
-		return ResponseEntity.status(HttpStatus.OK).body(response); // TODO: replace with return statement similar to in getSongById
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", String.format("PUT %s", Utils.getUrl(request)));
+		try {
+			String songId = params.get("songId");
+			String shouldDecrementStr = params.get("shouldDecrement");
+			if (songId == null || shouldDecrementStr == null) {
+				response.put("error", "Missing required parameters");
+				return Utils.setResponseStatus(response, null, null);
+			}
+			boolean shouldDecrement = Boolean.parseBoolean(shouldDecrementStr);
+			DbQueryStatus updateStatus = songDal.updateSongFavouritesCount(songId, shouldDecrement);
+			if (updateStatus.getdbQueryExecResult() == DbQueryExecResult.QUERY_OK) {
+				response.put("message", "Song favorites count updated successfully");
+				response.put("newFavouritesCount", updateStatus.getData());
+				return Utils.setResponseStatus(response, updateStatus.getdbQueryExecResult(), updateStatus.getData());
+			}
+			else if (updateStatus.getdbQueryExecResult() == DbQueryExecResult.QUERY_ERROR_NOT_FOUND) {
+				response.put("error", "Song not found");
+			}
+			else {
+				response.put("error", "Failed to update song favorites count");
+			}
+			return Utils.setResponseStatus(response, updateStatus.getdbQueryExecResult(), null);
+		}
+		catch (Exception e) {
+			response.put("error", "Internal Server Error");
+			return Utils.setResponseStatus(response, null, null);
+		}
 	}
 }
